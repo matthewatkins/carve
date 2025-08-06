b85b65682012bb781e4191e9c6a99539652e30c4
Matthew Atkins <matthew@atkins.codes>
2025-08-06 17:34:55 -0500

    working on versioning issue

--- packages/create-carve/src/index.ts
+++ packages/create-carve/src/index.ts
@@ -1,13 +1,20 @@
 #!/usr/bin/env node
 
+import { readFileSync } from "node:fs";
+import { join } from "node:path";
 import { defineCommand, runMain } from "citty";
 import { create } from "./commands/create";
 import { init } from "./commands/init";
 
+// Read version from package.json
+const packageJson = JSON.parse(
+	readFileSync(join(__dirname, "../package.json"), "utf-8"),
+);
+
 const main = defineCommand({
 	meta: {
 		name: "create-carve",
-		version: "0.1.0",
+		version: packageJson.version,
 		description: "CLI tool for creating new Carve projects",
 	},
 	subCommands: {
